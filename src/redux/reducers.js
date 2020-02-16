892b4204bd15a4ba53243c5790665f24400ca3b5
Casey Calkins <c.angelcalkins@outlook.com>
2020-02-16 10:36:51 -0600

    Almost done

--- src/redux/reducers.js
+++ src/redux/reducers.js
@@ -1,9 +1,20 @@
 import { combineReducers } from 'redux'
 
 const user = (state = null) => state
+// const newState = [...state];
+// newState.splice(action.value, 1);
+// return newState;
 
 // add switch statements in here
 const cars = (state = [], action) => {
+    switch(action.type) {
+        case 'ADD_CAR':
+            return [...state, action.payload]
+        case 'REMOVE_CAR':
+            const newState = [...state];
+            return newState.splice(action.payload, 1);
+            
+    }
     return state
 }
 
